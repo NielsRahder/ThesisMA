045fa8fdd9ad5fbc216b10ce54bbaddbd46980dd
NielsRahder <N.J.Rahder@tilburguniversity.edu>
2023-10-31 21:26:58 +0100

    add try-out code for BERT

--- sc/analysis/BERT.py
+++ sc/analysis/BERT.py
@@ -1,17 +1,28 @@
+import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
+
+df = pd.read_csv('../Data/ColdPlay.csv')
 
 # Load the tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
-# Tokenize the input sentence
-tokens = tokenizer.encode('I do not know how i feel about this', return_tensors='pt')
-print(tokens)
+# Function to get sentiment score
+def get_sentiment(lyric):
+    # Convert non-string inputs to empty strings
+    if not isinstance(lyric, str):
+        lyric = ""
 
-# Pass the tokenized sentence through the model
-outputs = model(tokens)
-predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
-predicted_class = torch.argmax(predictions).item() + 1
+    # Encode the lyric text
+    tokens = tokenizer.encode(lyric, return_tensors='pt', truncation=True, max_length=512)  # Truncate to handle very long lyrics
+    outputs = model(tokens)
+    predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+    predicted_class = torch.argmax(predictions).item() + 1
+    return predicted_class
 
-print(f"Sentiment score: {predicted_class}")
+# Apply the sentiment analysis function on the 'Lyric' column
+df['sentiment'] = df['Lyric'].apply(get_sentiment)
+
+# Save the modified DataFrame to a new CSV
+df.to_csv('../Data/ColdPlay_with_Sentiment.csv', index=False)
